6573f524b99962d3d9f31fcda817363f5e6d8c41
Andrew Marfo <andrewmarfo23@gmail.com>
2023-09-19 23:36:35 +0000

    responsiveness(Recpecting the \softkeyboard insects)

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -57,61 +57,68 @@ class _NewTransactionState extends State<NewTransaction> {
     return // input card
         Card(
       elevation: 3,
-      child: Container(
-        margin: const EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            // title
-            TextField(
-              decoration: const InputDecoration(
-                labelText: 'Title',
+      child: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(
+            top: 10,
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              // title
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Title',
+                ),
+                controller: titleController,
+                onSubmitted: (value) => submitData(),
               ),
-              controller: titleController,
-              onSubmitted: (value) => submitData(),
-            ),
-            // amount
-            TextField(
-              decoration: const InputDecoration(
-                labelText: 'Amount',
+              // amount
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Amount',
+                ),
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+                controller: amountController,
+                onSubmitted: (value) => submitData(),
               ),
-              keyboardType:
-                  const TextInputType.numberWithOptions(decimal: true),
-              controller: amountController,
-              onSubmitted: (value) => submitData(),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    _selectedDate == null
-                        ? 'No date Choosen'
-                        : 'Chosen Date: ${DateFormat.yMMMEd().format(_selectedDate!)}',
-                    style: Theme.of(context).textTheme.labelMedium,
-                  ),
-                  TextButton(
-                    onPressed: _datePicker,
-                    child: Text(
-                      'Choose date',
-                      style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                      ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      _selectedDate == null
+                          ? 'No date Choosen'
+                          : 'Chosen Date: ${DateFormat.yMMMEd().format(_selectedDate!)}',
+                      style: Theme.of(context).textTheme.labelMedium,
                     ),
-                  )
-                ],
+                    TextButton(
+                      onPressed: _datePicker,
+                      child: Text(
+                        'Choose date',
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-            // add transaction
-            ElevatedButton(
-              onPressed: submitData,
-              child: Text(
-                'Add Transaction',
-                style: Theme.of(context).textTheme.labelLarge,
-              ),
-            )
-          ],
+              // add transaction
+              ElevatedButton(
+                onPressed: submitData,
+                child: Text(
+                  'Add Transaction',
+                  style: Theme.of(context).textTheme.labelLarge,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
